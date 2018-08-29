dab60b2a139f484d8869036435eb9edd21ffd6af
591807572@qq.com <591807572@qq.com>
2018-08-29 11:47:31 +0800

    Initial Commit

---
+++ WXWLuckView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXWLuckView
+//
+//  Created by administrator on 2018/8/29.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXWLuckView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WXWLuckView
+//
+//  Created by administrator on 2018/8/29.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
