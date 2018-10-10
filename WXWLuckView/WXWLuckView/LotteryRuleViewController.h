6468a07d61a4851c7684e5ec519e312951be49f8
591807572@qq.com <591807572@qq.com>
2018-10-10 15:28:44 +0800

    add lottery rule

---
+++ WXWLuckView/WXWLuckView/LotteryRuleViewController.h
@@ -0,0 +1,17 @@
+//
+//  LotteryRuleViewController.h
+//  WXWLuckView
+//
+//  Created by administrator on 2018/10/10.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LotteryRuleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WXWLuckView/WXWLuckView/LotteryRuleViewController.m
@@ -0,0 +1,32 @@
+//
+//  LotteryRuleViewController.m
+//  WXWLuckView
+//
+//  Created by administrator on 2018/10/10.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import "LotteryRuleViewController.h"
+
+@interface LotteryRuleViewController ()
+
+@end
+
+@implementation LotteryRuleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
